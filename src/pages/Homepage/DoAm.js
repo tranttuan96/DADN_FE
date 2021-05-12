80c4ac63d2d3bb94d121fe301d9174d81b46d79d
Phan Văn Trường <truongfct.040920@gmail.com>
2021-05-12 23:52:16 +0700

    Update advanced views according to day, month, year in Do Am page

--- src/pages/Homepage/DoAm.js
+++ src/pages/Homepage/DoAm.js
@@ -1,65 +1,109 @@
-import React from 'react'
+import React, {useState, Component} from 'react'
 import Show_graph from '../utilities/show_graph'
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
+import SetThresholdForm from '../utilities/SetThresholdForm'
 // npm install @material-ui/core
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
 
 
-export default function DoAm(props) {
-    const classes = useStyles();
 
-    return ( 
-            //  <Show_graph />
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={12}>
+class DoAm extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        isAdvancedViews: false,
+        isDay: false,
+        isMonth: false,
+        isYear: false
+      };
+    }
+   
+    viewByDay = () => {
+      this.setState({
+        isAdvancedViews: true,
+        isDay: true,
+        isMonth: false,
+        isYear: false
+      });
+    }
+    viewByMonth = () => {
+      this.setState({
+        isAdvancedViews: true,
+        isDay: false,
+        isMonth: true,
+        isYear: false
+      });
+    }
+    viewByYear = () => {
+      this.setState({
+        isAdvancedViews: true,
+        isDay: false,
+        isMonth: false,
+        isYear: true
+      });
+    }
+    backMainView = ()=>{
+      this.setState({
+        isAdvancedViews: false
+      });
+    }
 
-
-        </Grid>
-        <Grid item xs={7}>
-          <Show_graph> </Show_graph>
-        </Grid>
-        
-        <Grid item xs={5} sm={3}>
-            <center>
-                <strong>Thông số độ ẩm hiện tại</strong>
-                <Paper className={classes.paper}>
-                    <Grid item xs={12} sm={12}>
-                    <strong>Ngưỡng trên</strong>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                    <Paper className={classes.paper}>162 mp</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                    <strong>Ngưỡng dưới</strong>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                    <Paper className={classes.paper}>94 mp</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                    <strong>Độ ẩm</strong>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                    <Paper className={classes.paper}>112 mp</Paper>
-                    </Grid>
-                </Paper>
-            </center>
-        </Grid>
-        
-      </Grid>
-    </div>
-    )
+    data_normal = [43, 42, 42.5, 41, 41, 40.5];
+    data_day = [42, 43, 42, 44, 45, 47];
+    data_month = [44, 45, 43, 42, 41, 42];
+    data_year = [40, 42, 43, 42, 44, 43];
+    labels_normal= ['7h00', '8h00', '9h00', '10h00', '11h00', '12h00'];
+    labels_day= ['20/04', '21/04', '22/04', '23/04', '24/04', '25/04'];
+    labels_month= ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6'];
+    labels_year= ['2015', '2016', '2017', '2018', '2019', '2020'];
+    render(){
+      var {isAdvancedViews, isDay, isMonth, isYear} = this.state;
+      var formSet = isAdvancedViews? '':<SetThresholdForm />;
+      var datas = isDay? this.data_day : (isMonth? this.data_month: (isYear? this.data_year: this.data_normal));
+      var labels = isDay? this.labels_day : (isMonth? this.labels_month: (isYear? this.labels_year: this.labels_normal));
+      return ( 
+        //  <Show_graph />
+        <div >
+          <Grid container spacing={3}>
+            <Grid item xs={10} sm={12}>
+              
+            </Grid>
+            
+            <Grid item xs={isAdvancedViews? 12:7}>
+              <center>
+                <Button variant="contained" color="primary" onClick={ this.viewByDay }>
+                  Ngày
+                </Button>
+                <Button variant="contained" color="primary" onClick={ this.viewByMonth }>
+                  Tháng
+                </Button>
+                <Button variant="contained" color="primary" onClick={ this.viewByYear }>
+                  Năm
+                </Button>
+              </center>
+              {
+                isAdvancedViews?
+                <left>
+                <Button variant="contained" color='secondary' onClick={ this.backMainView }>
+                  Trở lại
+                </Button>
+              </left>
+              : ''
+              } 
+              <Show_graph data={datas} label={labels}  />
+            </Grid>
+            
+            {formSet}
+            
+          </Grid>
+        </div>
+        )
+    }
+    
 }
+
+export default DoAm;
--- src/pages/utilities/show_graph.js
+++ src/pages/utilities/show_graph.js
@@ -1,72 +1,78 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Pie, Line, Bar, defaults } from 'react-chartjs-2'
 
 // Run command "npm install --save react-chartjs-2 chart.js" to install 
 
-
-// defaults.global.tooltips.enabled = false
-// defaults.global.legend.position = 'bottom'
-
-const Show_graph = () => {
-  return (
-    <div>
-      <Line
-        data={{
-          labels: ['20/04', '21/04', '22/04', '23/04', '24/04', '25/04'],
-          title: "a",
-          datasets: [
-            {
-              label: 'Humidity',
-              data: [12, 19, 7, 5, 13, 10],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            },
-            // {
-            //   label: 'Quantity',
-            //   data: [47, 52, 67, 58, 9, 50],
-            //   backgroundColor: 'orange',
-            //   borderColor: 'red',
-            // },
-          ],
-        }}
-        height={500}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [
+class Show_graph extends Component {
+  render(){
+    return (
+      <div>
+        <Line
+          data={{
+            labels: this.props.label,
+            title: "ABCDEF",
+            datasets: [
               {
-                ticks: {
-                  beginAtZero: true,
-                },
+                label: 'Độ ẩm',
+                data: this.props.data,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
               },
+              // {
+              //   label: 'Quantity',
+              //   data: [47, 52, 67, 58, 9, 50,30,30],
+              //   backgroundColor: 'orange',
+              //   borderColor: 'red',
+              // },
             ],
-          },
-          legend: {
-            labels: {
-              fontSize: 25,
+          }}
+          height={500}
+          width={600}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                  
+              }],
+              xAxes: [{
+                
+                scaleLabel: {
+                  display: true,
+                  labelString: 'ABC',
+                }
+              }],
             },
-          },
-        }}
-      />
-    </div>
-  )
+            legend: {
+              labels: {
+                fontSize: 80,
+              },
+            },
+          }}
+        />
+      </div>
+    )
+  }
+  
 }
 
-export default Show_graph
+export default Show_graph;
+ 
  
