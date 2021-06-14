caaaddc8613562903f4320ca64a2d8ede4236aa8
Tran Thanh Tuan <tranttuan96@gmail.com>
2021-06-15 03:38:39 +0700

    cap nhat giao dien va thong bao trang dang nhap

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {userLogin} from '../../setting/config'
 import { qlNguoiDungService } from "../../services/quanLyNguoiDungService"
 import '../../assets/scss/Layout/login.scss'
-
+import swal from 'sweetalert';
 
 export default function Login(props) {
 
@@ -41,17 +41,25 @@ export default function Login(props) {
         qlNguoiDungService.dangNhap(values).then(res => {
             console.log(res.data)
             if(!res.data) {
-                alert("wrong info");
+                swal("Thất bại.", "Thông tin đăng nhập không chính xác", "error", {
+                    buttons: false,
+                    timer: 1500,
+                });
             }
             else {
                 localStorage.setItem(userLogin, JSON.stringify(res.data))
-                alert("login success");
-                if(res.data.type === "nguoiDung") {
-                    props.history.push('/')
-                }
-                else {
-                    props.history.push('/admin/canhbaomayhu')
-                }
+                swal("Thành công.", "Đăng nhập thành công thành công", "success", {
+                    buttons: false,
+                    timer: 1500,
+                });
+                setTimeout(function() {
+                    if(res.data.type === "nguoiDung") {
+                        props.history.push('/')
+                    }
+                    else {
+                        props.history.push('/admin/canhbaomayhu')
+                    }
+                }, 1500)
             }
             
         }).catch(error => {
@@ -79,11 +87,10 @@ export default function Login(props) {
             <form onSubmit={handleSubmit}>
                 <div className="input-field">
                     <input name="username" type="text" required onChange={handleChange}/>
-                    <label>Email or Username</label>
+                    <label>Username</label>
                 </div>
                 <div className="input-field">
                     <input name="password" className="pswrd" type="password" required onChange={handleChange}/>
-                    <span className="show">SHOW</span>
                     <label>Password</label>
                 </div>
                 <div className="button">
@@ -92,9 +99,6 @@ export default function Login(props) {
                     <button>LOGIN</button>
                 </div>
             </form>
-            <div className="signup">
-                Not a member? <a href="#">Signup now</a>
-            </div>
         </div>
     )
 }
